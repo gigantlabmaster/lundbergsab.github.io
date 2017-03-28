df01c3704e75f3c16e4d44b4198f8b598e44d1b4
emilingemarkarlsson@gmail.com <emilingemar@gmail.com>
2017-03-28 20:56:29 +0200

    UPDATE

---
+++ cache/gantry5/g5_helium/compiled/yaml/c3ab7f6fc0197af1eec795b1cdb607cb.yaml.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    '@class' => 'Gantry\\Component\\File\\CompiledYamlFile',
+    'filename' => '/Users/emilkarlsson/Code/lundbergsab/user/plugins/gantry5/engines/nucleus/particles/login.yaml',
+    'modified' => 1489131968,
+    'data' => [
+        'name' => 'Login',
+        'description' => 'Login Particle',
+        'type' => 'particle',
+        'icon' => 'fa-sign-in',
+        'form' => [
+            'fields' => [
+                'enabled' => [
+                    'type' => 'checkbox',
+                    'label' => 'Enabled',
+                    'description' => 'Globally enable login particles.',
+                    'default' => true
+                ]
+            ]
+        ]
+    ]
+];
