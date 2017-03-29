83e7e724821f6b6bec5f19befbb1aaff7053e934
emilingemarkarlsson@gmail.com <emilingemar@gmail.com>
2017-03-29 06:19:36 +0200

    commit

---
+++ cache/gantry5/g5_helium/html/180159e3d70899e6cdaa1e943090ac9c.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    '_type' => 'Gantry\\Component\\Content\\Block\\HtmlBlock',
+    '_version' => 1,
+    'id' => '58db35c8d01e01.43186046',
+    'content' => '<div class="g-content g-particle">
+                                <div id="g-mobilemenu-container" data-g-menu-breakpoint="48rem"></div>
+            
+    </div>'
+];
---
+++ cache/gantry5/g5_helium/html/e93dd05e66aef97c805fede7d2569fa2.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    '_type' => 'Gantry\\Component\\Content\\Block\\HtmlBlock',
+    '_version' => 1,
+    'id' => '58db35c8cfaf51.50596691',
+    'content' => '<div class="g-content g-particle">
+                            <div class="">
+    <div class="g-totop">
+        <a href="#" id="g-totop" rel="nofollow">
+            Back to top            <i class="fa fa-chevron-up fa-fw"></i>                    </a>
+    </div>
+</div>
+            
+    </div>'
+];
