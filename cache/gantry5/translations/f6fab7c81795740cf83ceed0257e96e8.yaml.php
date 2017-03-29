eb27aa511837088a905a6f353afb29461ecd90f2
emilingemarkarlsson@gmail.com <emilingemar@gmail.com>
2017-03-29 06:41:14 +0200

    commit

---
+++ cache/gantry5/translations/f6fab7c81795740cf83ceed0257e96e8.yaml.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    '@class' => 'Gantry\\Component\\File\\CompiledYamlFile',
+    'filename' => 'gantry-admin://translations/en/nucleus.yaml',
+    'modified' => 1489131964,
+    'data' => [
+        'GANTRY5_ENGINE_SORRY_NO_CONTENT' => 'Sorry, no content',
+        'GANTRY5_ENGINE_UNKNOWN_ERROR' => 'Unknown error',
+        'GANTRY5_ENGINE_FORM_SUMMARY' => 'Here is the summary of what you wrote to us:',
+        'GANTRY5_ENGINE_BLOG_ITEM_NEXT_POST' => 'Next Post',
+        'GANTRY5_ENGINE_BLOG_ITEM_PREV_POST' => 'Previous Post',
+        'GANTRY5_ENGINE_READMORE' => 'Read More...'
+    ]
+];
