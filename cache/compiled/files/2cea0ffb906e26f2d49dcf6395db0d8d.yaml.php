c34ec7a3badc2f4b8c2776c5811f61d324697063
emilingemarkarlsson@gmail.com <emilingemar@gmail.com>
2017-03-29 06:23:22 +0200

    update

--- cache/compiled/files/2cea0ffb906e26f2d49dcf6395db0d8d.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'themes://g5_helium/g5_helium.yaml',
-    'modified' => 1489131972,
-    'data' => [
-        'enabled' => true
-    ]
-];
