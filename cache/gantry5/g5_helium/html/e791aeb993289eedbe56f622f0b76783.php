b225605594a99a86c6b66741f113b3a95b73c99a
emilingemarkarlsson@gmail.com <emilingemar@gmail.com>
2017-03-29 06:12:40 +0200

    update

---
+++ cache/gantry5/g5_helium/html/e791aeb993289eedbe56f622f0b76783.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    '_type' => 'Gantry\\Component\\Content\\Block\\HtmlBlock',
+    '_version' => 1,
+    'id' => '58db342030d930.22650900',
+    'content' => '<div class="g-content g-particle">
+                            <div class="g-copyright ">
+    &copy;
+            Gantry Framework
+        2016 -     2017
+    <br />Developed by RocketTheme exclusively<br />for Gantry 5.</div>
+            
+    </div>'
+];
