dd1bdb26fbbd70d9c4d3f89b4b1e76ec71529105
emilingemarkarlsson@gmail.com <emilingemar@gmail.com>
2017-03-29 06:35:58 +0200

    update

---
+++ cache/gantry5/g5_helium/html/3163afe03af855405d6fefe2cc59a9f8.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    '_type' => 'Gantry\\Component\\Content\\Block\\HtmlBlock',
+    '_version' => 1,
+    'id' => '58db3984a287f8.13900722',
+    'content' => '<div class="g-content g-particle">
+                                <div id="g-mobilemenu-container" data-g-menu-breakpoint="48rem"></div>
+            
+    </div>'
+];
---
+++ cache/gantry5/g5_helium/html/d4a1287b7ae419a1bdee5fb5bf4d11f5.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    '_type' => 'Gantry\\Component\\Content\\Block\\HtmlBlock',
+    '_version' => 1,
+    'id' => '58db3984975a27.16135716',
+    'content' => '<div class="g-content g-particle">
+                            <div class="g-copyright ">
+    &copy;
+            Gantry Framework
+        2016 -     2017
+    <br />Developed by RocketTheme exclusively<br />for Gantry 5.</div>
+            
+    </div>'
+];
