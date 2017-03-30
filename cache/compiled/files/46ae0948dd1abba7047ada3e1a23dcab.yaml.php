1ed1da118d87754943a5e901b1d35a1353e2dcfa
emilingemarkarlsson@gmail.com <emilingemar@gmail.com>
2017-03-30 20:06:36 +0200

    commit

---
+++ cache/compiled/files/46ae0948dd1abba7047ada3e1a23dcab.yaml.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/emilkarlsson/Code/lundbergsab/user/accounts/admin.yaml',
+    'modified' => 1490889089,
+    'data' => [
+        'email' => 'emilingemarkarlsson@gmail.com',
+        'fullname' => 'Emil Karlsson',
+        'title' => 'admin',
+        'state' => 'enabled',
+        'access' => [
+            'admin' => [
+                'login' => true,
+                'super' => true
+            ],
+            'site' => [
+                'login' => true
+            ]
+        ],
+        'hashed_password' => '$2y$10$iu7zWjedFYKSGzqa3AXvv.mXJoOBOdXrfh8TF/r1XNJdWl8j0ssRW'
+    ]
+];
